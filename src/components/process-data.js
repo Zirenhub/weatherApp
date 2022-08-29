38afe811d1cee2de904908a21da8e40369981fcd
Erdinch <erdinchorhan02@gmail.com>
2022-08-29 17:36:06 +0300

    return object w/ info

--- src/components/process-data.js
+++ src/components/process-data.js
@@ -1,11 +1,20 @@
 class processData {
-  constructor(mainData) {
+  constructor(mainData, sysData, cloudsData, placeName) {
     this.feelsLike = mainData.feels_like;
     this.humidity = mainData.humidity;
     this.pressure = mainData.pressure;
     this.temp = mainData.temp;
     this.tempMax = mainData.temp_max;
     this.tempMin = mainData.temp_min;
+
+    this.country = sysData.country;
+    this.sunrise = sysData.sunrise;
+    this.sunset = sysData.sunset;
+
+    this.desc = cloudsData[0].description;
+    this.mainDesc = cloudsData[0].main;
+
+    this.place = placeName;
   }
 
   getFeelsLike() {
@@ -30,6 +39,14 @@ class processData {
 
   getTempMin() {
     return this.tempMin;
+  }
+
+  getDesc() {
+    return this.desc;
+  }
+
+  getPlace() {
+    return this.place;
   }
 }
 
