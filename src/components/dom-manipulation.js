beec243a480862ef7ce7d3cc689389705f854f82
Erdinch <erdinchorhan02@gmail.com>
2022-08-30 00:20:18 +0300

    humidity and switching units

--- src/components/dom-manipulation.js
+++ src/components/dom-manipulation.js
@@ -6,11 +6,15 @@ const searchBar = document.querySelector('#searchBar');
 const searchButton = document.querySelector('#searchButton');
 
 const tempDisplay = document.querySelector('#temp');
+const feelsLike = document.querySelector('#feelsLike');
+const humidity = document.querySelector('#humidity');
+const skyInfo = document.querySelector('#skyInfo');
+
+const switchButton = document.querySelector('#switchButton');
 
 const locationDisplay = document.querySelector('#locationName');
-const skyInfo = document.querySelector('#skyInfo');
-const timeInfo = document.querySelector('#timeInfo');
 const countryInfo = document.querySelector('#countryInfo');
+const timeInfo = document.querySelector('#timeInfo');
 
 const bindEvents = () => {
   searchBar.addEventListener('keypress', (e) => {
@@ -22,14 +26,36 @@ const bindEvents = () => {
   searchButton.addEventListener('click', () => {
     getWeatherInfo(searchBar);
   });
+
+  switchButton.addEventListener('click', (e) => {
+    let target = e.target;
+    switchUnit(target);
+  });
+};
+
+const switchUnit = (target) => {
+  if (target.textContent === '°F') {
+    tempDisplay.childNodes[2].textContent =
+      (tempDisplay.childNodes[2].textContent * 9) / 5 + 32;
+  }
 };
 
 const pageContent = (newLocation) => {
   tempDisplay.childNodes[2].remove();
+  feelsLike.childNodes[2].remove();
+  humidity.childNodes[2].remove();
 
   const pTemp = document.createElement('p');
   pTemp.textContent = newLocation.getTemp();
   tempDisplay.insertBefore(pTemp, tempDisplay.childNodes[2]);
+
+  const pFeelsLike = document.createElement('p');
+  pFeelsLike.textContent = newLocation.getFeelsLike();
+  feelsLike.insertBefore(pFeelsLike, feelsLike.childNodes[2]);
+
+  const pHumidity = document.createElement('p');
+  pHumidity.textContent = newLocation.getHumidity() + '%';
+  humidity.insertBefore(pHumidity, humidity.childNodes[2]);
 
   locationDisplay.textContent = newLocation.getPlace();
   skyInfo.textContent = newLocation.getDesc();
@@ -57,10 +83,11 @@ const pageContent = (newLocation) => {
       `${cityTimeZone}`,
       'yyyy-MM-dd HH:mm:ss zzz'
     );
-    timeInfo.textContent = time;
     countryInfo.textContent = country;
+    timeInfo.textContent = time;
   } else {
-    // could not find city.
+    countryInfo.textContent = newLocation.getCountry();
+    timeInfo.textContent = 'could not find city time';
   }
 };
 
