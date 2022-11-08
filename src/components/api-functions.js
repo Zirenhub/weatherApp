7545417a414bea7c9def248bc64ab5002937ba53
Zirenhub <85786109+Zirenhub@users.noreply.github.com>
2022-11-08 22:32:45 +0200

    fixed production build bug

--- src/components/api-functions.js
+++ src/components/api-functions.js
@@ -3,7 +3,7 @@ import { errorHandling } from './error-handling';
 const fetchWeatherInfo = async (location, unit) => {
   try {
     const weatherAPI = await fetch(
-      `http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=3753fea3c9f037d32f1dec1ad67ef169&units=${unit}`,
+      `https://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=3753fea3c9f037d32f1dec1ad67ef169&units=${unit}`,
       { mode: 'cors' }
     );
     if (weatherAPI.ok) {
